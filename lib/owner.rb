c65347ba13357637de8921737cf066221258874f
Sergey Zolotukhin <zolotukhinsereg@gmail.com>
2020-04-22 02:41:01 +0000

    Done.

--- lib/owner.rb
+++ lib/owner.rb
@@ -1,3 +1,20 @@
 class Owner
-  # code goes here
+  
+  @@all = []
+  
+  attr_accessor :cat, :dog
+  attr_reader :name, :species
+  
+  def initiliaze(name)
+    @name = name
+    @species = species
+    @cat = cat
+    @dog = dog
+    @all << self
+  end
+  
+  def self.all
+    @@all
+  end
+  
 end
