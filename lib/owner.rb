b76a92edc567156f63a22ea51b8164e648ecb54f
Sergey Zolotukhin <zolotukhinsereg@gmail.com>
2020-04-23 03:40:12 +0000

    Automatically backed up by Learn

--- lib/owner.rb
+++ lib/owner.rb
@@ -44,11 +44,11 @@ class Owner
   end
   
   def buy_cat(name)
-    new_cat = Cat.new(name, self)
+    Cat.new(name, self)
   end
   
   def buy_dog(name)
-    new_dog = Dog.new(name, self)
+    Dog.new(name, self)
   end
   
   def walk_dogs
